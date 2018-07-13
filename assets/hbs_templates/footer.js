10176f45a7aab453fcb6523408704f3ae457da67
Erik Taylor <etaylor@usptm013.na.acushnetgolf.com>
2018-07-13 16:44:28 -0700

    favicon & footer changes

--- assets/hbs_templates/footer.js
+++ assets/hbs_templates/footer.js
@@ -5,5 +5,5 @@ define(["handlebars"], function(Handlebars) { return Handlebars.template({"compi
     + alias4(((helper = (helper = helpers.footer_background_color || (depth0 != null ? depth0.footer_background_color : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"footer_background_color","hash":{},"data":data}) : helper)))
     + "\">\n	<div class=\"panel_body\">\n		<div class=\"credits\">&copy;"
     + alias4(((helper = (helper = helpers.footer_text || (depth0 != null ? depth0.footer_text : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"footer_text","hash":{},"data":data}) : helper)))
-    + "</div>\n		<div class=\"wedge-2x\"></div>\n	</div>\n</footer>";
+    + " All Rights Reserved</div>\n		<div class=\"wedge-2x\"></div>\n	</div>\n</footer>";
 },"useData":true}); });
