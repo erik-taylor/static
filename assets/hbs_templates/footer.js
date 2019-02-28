5ea10055cb49fb63d7bc40f92d8959fa1757e37a
Erik Taylor <etaylor@usptm013.na.acushnetgolf.com>
2019-02-27 16:21:51 -0800

    dynamic copyright in footer

--- assets/hbs_templates/footer.js
+++ assets/hbs_templates/footer.js
@@ -1,9 +1,7 @@
 define(["handlebars"], function(Handlebars) { return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+    var helper;
 
   return "<footer class=\"panel color_4 dark text_center\" style=\"background-color: "
-    + alias4(((helper = (helper = helpers.footer_background_color || (depth0 != null ? depth0.footer_background_color : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"footer_background_color","hash":{},"data":data}) : helper)))
-    + ";\">\n	<div class=\"panel_body\">\n		<div class=\"credits\">&copy;"
-    + alias4(((helper = (helper = helpers.footer_text || (depth0 != null ? depth0.footer_text : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"footer_text","hash":{},"data":data}) : helper)))
-    + " All Rights Reserved</div>\n		<div class=\"wedge-2x\"></div>\n	</div>\n</footer>";
+    + container.escapeExpression(((helper = (helper = helpers.footer_background_color || (depth0 != null ? depth0.footer_background_color : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"footer_background_color","hash":{},"data":data}) : helper)))
+    + ";\">\n	<div class=\"panel_body\">\n		<div class=\"credits\">\n			&copy; \n			\n			<span id=\"copyright\">\n        <script>document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))</script>\n    	</span> \n\n    	All Rights Reserved\n		</div>\n		<div class=\"wedge-2x\"></div>\n	</div>\n</footer>";
 },"useData":true}); });
